22a5b4958374f57872b5be43ee087309264ab119
Omer-Cicek <omercicek160408@gmail.com>
2022-04-18 19:39:49 +0300

    Private router added

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,11 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+  Outlet,
+} from 'react-router-dom';
 import Main from '../pages/Main';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Navbar from '../components/Navbar';
 import MovieDetails from '../pages/MovieDetails';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 const AppRouter = () => {
+  const { currentUser } = useContext(AuthContext);
+  function PrivateRouter() {
+    let location = useLocation();
+    if (!currentUser) {
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+    return <Outlet />;
+  }
   return (
     <Router>
       <Navbar />
@@ -13,7 +30,9 @@ const AppRouter = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/details/:id" element={<MovieDetails />} />
+        <Route element={<PrivateRouter />}>
+          <Route path="/details/:id" element={<MovieDetails />} />
+        </Route>
       </Routes>
     </Router>
   );
